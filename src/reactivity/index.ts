5860e9b236e56c6b40b34f30c0e39df424286b66
alexzhang1030 <1642114555@qq.com>
2022-03-07 22:55:29 +0800

    chore(start) setup project

--- src/reactivity/index.ts
+++ src/reactivity/index.ts
@@ -1,3 +1,3 @@
-export const sum = function (a: number, b: number) {
+export function sum(a, b) {
   return a + b
 }
---
+++ src/reactivity/tests/index.spec.ts
@@ -0,0 +1,5 @@
+import { sum } from '../index'
+
+it('init', () => {
+  expect(sum(2, 3)).toBe(5)
+})
