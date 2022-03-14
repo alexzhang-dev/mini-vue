18d24b2673247702356a6fa2bae6f96f8c2df233
alexzhang1030 <1642114555@qq.com>
2022-03-14 21:26:39 +0800

    chore: add helloworld example

---
+++ example/helloworld/App.js
@@ -0,0 +1,10 @@
+export default {
+  render() {
+    return h('div', 'hi ' + this.title)
+  },
+  setup() {
+    return {
+      title: 'mini-vue',
+    }
+  },
+}
---
+++ example/helloworld/main.js
@@ -0,0 +1,3 @@
+import App from './App.js'
+
+createApp(App).mount(document.querySelector('#app'))
