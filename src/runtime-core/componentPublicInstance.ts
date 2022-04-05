8468aba7902c0ea489f416873edeae8bc73b5125
alexzhang1030 <1642114555@qq.com>
2022-04-05 15:27:01 +0800

    feat(runtime-core): add props public getter

--- src/runtime-core/componentPublicInstance.ts
+++ src/runtime-core/componentPublicInstance.ts
@@ -3,6 +3,7 @@ import { hasOwn } from '../shared/index'
 const PublicProxyGetterMapping = {
   $el: i => i.vnode.el,
   $slots: i => i.slots,
+  $props: i => i.props,
 }
 
 export const componentPublicInstanceProxyHandlers = {
