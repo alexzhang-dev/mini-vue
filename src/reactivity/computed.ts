2232d565f55216762859191a2e686aa7252361e4
alexzhang <1642114555@qq.com>
2022-04-08 16:04:16 +0800

    chore: update computed

--- src/reactivity/computed.ts
+++ src/reactivity/computed.ts
@@ -3,21 +3,21 @@ import { ReactiveEffect } from './effect'
 class ComputedRefImpl {
   private _getter: any
   private _value: any
-  private _dirty = false
+  private _dirty = true
   private _effect: any
   constructor(getter) {
     this._getter = getter
     this._effect = new ReactiveEffect(getter, {
       scheduler: () => {
         // 在 scheduler 中把锁打开
-        this._dirty = false
+        this._dirty = true
       },
     })
   }
   get value() {
-    if (!this._dirty) {
+    if (this._dirty) {
       this._value = this._effect.run()
-      this._dirty = true
+      this._dirty = false
     }
     return this._value
   }
