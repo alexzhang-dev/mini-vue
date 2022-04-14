c81e6129fc1aa3880fa799b1d5588db867f68632
alexzhang <1642114555@qq.com>
2022-04-14 16:22:13 +0800

    type: runtime-dom type

--- src/runtime-dom/index.ts
+++ src/runtime-dom/index.ts
@@ -1,41 +1,47 @@
 import { createRenderer } from '../runtime-core'
+import { EventHandler, PropsValue } from '../runtime-core/types/element'
+import { VNode } from '../runtime-core/vnode'
+import type { ElementType, NormalNode } from './types'
 
-function createElement(type) {
+function createElement(type: ElementType) {
   return document.createElement(type)
 }
 
 const isOn = (key: string) => /^on[A-Z]/.test(key)
 
-function patchProp(el, prop, val) {
+function patchProp(e: NormalNode, prop: string | keyof HTMLElementEventMap, val: PropsValue) {
+  const el = e as HTMLElement
   if (isOn(prop)) {
-    const event = prop.slice(2).toLowerCase()
-    el.addEventListener(event, val)
+    const event = prop.slice(2).toLowerCase() as keyof HTMLElementEventMap
+    el.addEventListener(event, val as EventHandler)
   } else {
     if (val === undefined || null) el.removeAttribute(prop)
-    else el.setAttribute(prop, val)
+    else el.setAttribute(prop, val as string)
   }
 }
 
-function insert(el, parent, anchor) {
-  parent.insertBefore(el, anchor || null)
+function insert<T extends NormalNode>(el: T, parent: T, anchor: T) {
+  (parent as HTMLElement).insertBefore((el as HTMLElement), anchor || null)
 }
 
-function selector(container) {
+function selector(container: string) {
   return document.querySelector(container)
 }
 
-function remove(child) {
+function remove(c: NormalNode) {
+  const child = c as HTMLElement
   const parentElement = child.parentNode
   if (parentElement) {
     parentElement.removeChild(child)
   }
 }
 
-function setElementText(el, text) {
+function setElementText(e: NormalNode, text: string) {
+  const el = e as HTMLElement
   el.textContent = text
 }
 
-const renderer: any = createRenderer({
+const renderer = createRenderer({
   createElement,
   patchProp,
   insert,
@@ -44,8 +50,8 @@ const renderer: any = createRenderer({
   setElementText,
 })
 
-export const createApp = (...args) => {
-  return renderer.createApp(...args)
+export const createApp = (rootComponent: VNode) => {
+  return renderer.createApp(rootComponent)
 }
 
 export * from '../runtime-core'
