4ce9214b5d2c6f6ace50c326b0ec7f470c153451
alexzhang <1642114555@qq.com>
2022-04-14 18:23:08 +0800

    chore: add desc about renderSlots

--- src/runtime-core/helpers/renderSlots.ts
+++ src/runtime-core/helpers/renderSlots.ts
@@ -3,7 +3,7 @@ import { InternalSlots } from '../types/slots'
 import { Fragment } from '../vnode'
 
 export function renderSlots(slots: InternalSlots, name: string = 'default', props: unknown) {
-  // 此时 slots 就是 Object
+  // slots 只能是 Object
   const slot = slots[name]
   if (slot) {
     if (typeof slot === 'function') {
